90e9b7aab59cc429df7039a22b42f3fe91a7546f
Guilherme Tavares <zetavares.rib@gmail.com>
2019-06-11 10:19:43 -0300

    merge

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,47 +5,68 @@ from .models import Pilot
 
 
 def test_models_pilot_print_drivers(data):
-    assert len(Pilot.print_drivers(process_drivers(data, Pilot))) == 10
+    if not len(Pilot.print_drivers(process_drivers(data, Pilot))) == 10:
+        raise AssertionError()
 
 
 def test_utils_process_stdin(capsys, data):
     process_stdin(data, Pilot)
     captured = capsys.readouterr()
-    assert 'RESULTADO FINAL' in captured.out
+    if not 'RESULTADO FINAL' in captured.out:
+        raise AssertionError()
 
 
 def test_utils_pilot_process_drivers(data):
-    assert len(process_drivers(data, Pilot).keys()) == 6
+    if not len(process_drivers(data, Pilot).keys()) == 6:
+        raise AssertionError()
 
 
 def test_utils_format_bool():
-    assert format_bool(True) == 'SIM'
-    assert format_bool(False) == 'NAO'
+    if not format_bool(True) == 'SIM':
+        raise AssertionError()
+
+    if not format_bool(False) == 'NAO':
+        raise AssertionError()
 
 
 def test_utils_format_to_timedelta():
     value = format_to_timedelta('1:02.852')
-    assert value.seconds == 62
+    if not value.seconds == 62:
+        raise AssertionError()
 
 
 def test_utils_format_to_int():
-    assert format_to_int('1') == 1
-    assert format_to_int('t') == None
+    if not format_to_int('1') == 1:
+        raise AssertionError()
+
+    if not format_to_int('t') == None:
+        raise AssertionError()
 
 
 def test_utils_format_to_float():
-    assert format_to_float('12,2') == 12.2
+    if not format_to_float('12,2') == 12.2:
+        raise AssertionError()
 
 
 def test_utils_format_line():
     line = '23:49:08.277      038 – F.MASSA                           1		1:02.852                        44,275'
     date, code, _, name, lap, time_, speed = format_line(line)
 
-    assert code == '038'
-    assert name == 'F.MASSA'
-    assert lap == 1
-    assert time_.seconds == 62
-    assert speed == 44.275
+    if not code == '038':
+        raise AssertionError()
+
+    if not name == 'F.MASSA':
+        raise AssertionError()
+
+    if not lap == 1:
+        raise AssertionError()
+
+    if not time_.seconds == 62:
+        raise AssertionError()
+
+    if not speed == 44.275:
+        raise AssertionError()
 
     line = 'NOT A VALID LINE'
-    assert len(format_line(line)) == 0
+    if not len(format_line(line)) == 0:
+        raise AssertionError()
